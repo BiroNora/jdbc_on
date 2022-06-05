5e1496776fa642de1f98c0383e5fabe615fbcb03
BiroNora <52859737+BiroNora@users.noreply.github.com>
2022-06-05 19:52:23 +0200

    Role_photos changed, items added on data.txt

--- src/main/resources/db/migration/V4__PhotosTable.sql
+++ src/main/resources/db/migration/V4__PhotosTable.sql
@@ -1,8 +1,8 @@
 CREATE TABLE role_photos (
     photo_id BIGSERIAL PRIMARY key,
     url TEXT,
-    movie_id BIGSERIAL REFERENCES movie(movie_id),
-    actor_id BIGSERIAL REFERENCES actor(actor_id),
-    role_id BIGSERIAL REFERENCES plays(role_id),
+    movie_id BIGINT NULL,
+    actor_id BIGINT NULL,
+    role_id BIGINT NULL,
     unique (url)
 );
