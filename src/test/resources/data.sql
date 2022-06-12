9aabd554bf46366f0dd916962687188851e8dcd5
BiroNora <52859737+BiroNora@users.noreply.github.com>
2022-06-12 12:45:55 +0200

    Add first integration test

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -4,4 +4,9 @@ INSERT INTO actor(full_name, birth_date) VALUES('Jonathan Dep', '1963-06-09');
 --INSERT INTO actor(full_name, birth_date) VALUES('Johnny Depp3', '1963-06-09');
 --INSERT INTO actor(full_name, birth_date) VALUES('Johnny Depp4', '1963-06-09');
 
+INSERT INTO movie(title, title_original, release_date) VALUES('Karib tengör', 'Caribbien', '2003-07-09');
+
+INSERT INTO plays(role_name, movie_id, actor_id) VALUES ('Jack', 1, 1);
+
+
 
--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,6 +1,30 @@
 create TABLE actor (
-    actor_id BIGSERIAL PRIMARY key,
-    full_name VARCHAR(255) NOT null,
-    birth_date DATE NOT null ,
-    death_date DATE DEFAULT null
+    actor_id BIGSERIAL PRIMARY KEY,
+    full_name VARCHAR(255) NOT NULL,
+    birth_date DATE NOT NULL ,
+    death_date DATE DEFAULT NULL,
+    unique (full_name)
 );
+
+CREATE TABLE movie (
+    movie_id BIGSERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    title_original TEXT,
+    release_date DATE NOT NULL
+);
+
+CREATE TABLE plays (
+    role_id BIGSERIAL PRIMARY KEY,
+    role_name TEXT NOT NULL,
+    movie_id BIGSERIAL NOT NULL REFERENCES movie(movie_id),
+	actor_id BIGSERIAL NOT NULL REFERENCES actor(actor_id)
+);
+
+CREATE TABLE role_photos (
+    photo_id BIGSERIAL PRIMARY KEY,
+    url TEXT,
+    movie_id BIGINT NULL,
+    actor_id BIGINT NULL,
+    role_id BIGINT NULL
+);
+
