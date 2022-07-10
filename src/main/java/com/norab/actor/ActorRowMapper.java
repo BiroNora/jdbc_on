f831dc4697cccc658745346d15c9554def3ffba0
BiroNora <52859737+BiroNora@users.noreply.github.com>
2022-07-10 19:55:16 +0200

    use constructor with RowMapper

--- src/main/java/com/norab/actor/ActorRowMapper.java
+++ src/main/java/com/norab/actor/ActorRowMapper.java
@@ -10,17 +10,16 @@ public class ActorRowMapper implements RowMapper<Person> {
 
     @Override
     public Person mapRow(ResultSet resultSet, int i) throws SQLException {
-        Person actor = new Person();
         String death_date = resultSet.getString("death_date");
 
-        actor.setActorId(resultSet.getInt("actor_id"));
-        actor.setFullName(resultSet.getString("full_name"));
-        actor.setBirthDate(LocalDate.parse(resultSet.getString("birth_date")));
-        actor.setDeathDate(death_date == null
-            || death_date.compareTo("") == 0
-            || death_date.compareTo("0000-00-00") == 0
-            ? null : LocalDate.parse(death_date));
-
-        return actor;
+        return new Person (
+            resultSet.getInt("actor_id"),
+            resultSet.getString("full_name"),
+            LocalDate.parse(resultSet.getString("birth_date")),
+            death_date == null
+                || death_date.compareTo("") == 0
+                || death_date.compareTo("0000-00-00") == 0
+                ? null : LocalDate.parse(death_date)
+        );
     }
 }
