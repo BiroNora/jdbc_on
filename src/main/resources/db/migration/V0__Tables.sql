9480d78175b7fd5c17f3f1f5c1057056cc3b0ec1
BiroNora <52859737+BiroNora@users.noreply.github.com>
2022-07-14 20:43:53 +0200

    wip

--- src/main/resources/db/migration/V0__Tables.sql
+++ src/main/resources/db/migration/V0__Tables.sql
@@ -2,8 +2,7 @@ CREATE TABLE actors (
     actor_id SERIAL PRIMARY KEY,
     full_name VARCHAR(255) NOT NULL,
     birth_date SMALLINT NULL,
-    death_date SMALLINT DEFAULT NULL,
-    orig_actor_id TEXT NULL
+    death_date SMALLINT DEFAULT NULL
 );
 
 CREATE TABLE movies (
@@ -13,56 +12,55 @@ CREATE TABLE movies (
     release_date SMALLINT NOT NULL,
     end_date SMALLINT NULL,
     m_type TEXT,
-    is_adult BOOLEAN,
-    orig_movie_id TEXT NULL
+    is_adult BOOLEAN
 );
 
 CREATE TABLE plays (
     role_id SERIAL PRIMARY KEY,
     role_name VARCHAR(255) NOT NULL,
     movie_id INTEGER NULL,
-        actor_id INTEGER NULL,
-        CONSTRAINT fk_plays_movie_id
-            FOREIGN KEY (movie_id)
-            REFERENCES movies (movie_id)
-            ON DELETE SET NULL,
-        CONSTRAINT fk_plays_actor_id
-            FOREIGN KEY (actor_id)
-            REFERENCES actors (actor_id)
-            ON DELETE SET NULL
+    actor_id INTEGER NULL,
+    CONSTRAINT fk_plays_movie_id
+        FOREIGN KEY (movie_id)
+        REFERENCES movies (movie_id)
+        ON DELETE SET NULL,
+    CONSTRAINT fk_plays_actor_id
+        FOREIGN KEY (actor_id)
+        REFERENCES actors (actor_id)
+        ON DELETE SET NULL
 );
 
 CREATE TABLE photos (
     photo_id SERIAL PRIMARY KEY,
-    url TEXT,
+    url TEXT NOT NULL,
     movie_id INTEGER NULL,
     actor_id INTEGER NULL,
-    role_id  INTEGER NULL,
-        CONSTRAINT fk_photos_movie_id
-            FOREIGN KEY (movie_id)
-            REFERENCES movies (movie_id)
-            ON DELETE SET NULL,
-        CONSTRAINT fk_photos_actor_id
-            FOREIGN KEY (actor_id)
-            REFERENCES actors (actor_id)
-            ON DELETE SET NULL,
-        CONSTRAINT fk_photos_role_id
-            FOREIGN KEY (role_id)
-            REFERENCES plays (role_id)
-            ON DELETE SET NULL
+    role_id INTEGER NULL,
+    CONSTRAINT fk_photos_movie_id
+        FOREIGN KEY (movie_id)
+        REFERENCES movies (movie_id)
+        ON DELETE SET NULL,
+    CONSTRAINT fk_photos_actor_id
+        FOREIGN KEY (actor_id)
+        REFERENCES actors (actor_id)
+        ON DELETE SET NULL,
+    CONSTRAINT fk_photos_role_id
+        FOREIGN KEY (role_id)
+        REFERENCES plays (role_id)
+        ON DELETE SET NULL
 );
 
 CREATE TABLE directors (
     actor_id INTEGER,
     movie_id INTEGER,
     CONSTRAINT fk_directors_movie_id
-                FOREIGN KEY (movie_id)
-                REFERENCES movies (movie_id)
-                ON DELETE SET NULL,
+        FOREIGN KEY (movie_id)
+        REFERENCES movies (movie_id)
+        ON DELETE SET NULL,
     CONSTRAINT fk_directors_actor_id
-                FOREIGN KEY (actor_id)
-                REFERENCES actors (actor_id)
-                ON DELETE SET NULL,
+        FOREIGN KEY (actor_id)
+        REFERENCES actors (actor_id)
+        ON DELETE SET NULL,
     CONSTRAINT fk_directors PRIMARY KEY (actor_id, movie_id)
 );
 
@@ -70,8 +68,8 @@ CREATE TABLE genre (
     movie_id INTEGER NOT NULL,
     genre TEXT NOT NULL,
     CONSTRAINT fk_genre_movie_id
-                FOREIGN KEY (movie_id)
-                REFERENCES movies (movie_id)
-                ON DELETE SET NULL,
+        FOREIGN KEY (movie_id)
+        REFERENCES movies (movie_id)
+        ON DELETE SET NULL,
     CONSTRAINT fk_genre PRIMARY KEY (movie_id, genre)
 );
--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,7 +1,7 @@
-create TABLE actors (
+CREATE TABLE actors (
     actor_id SERIAL PRIMARY KEY,
     full_name VARCHAR(255) NOT NULL,
-    birth_date SMALLINT NOT NULL ,
+    birth_date SMALLINT NULL,
     death_date SMALLINT DEFAULT NULL
 );
 
@@ -9,18 +9,22 @@ CREATE TABLE movies (
     movie_id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
     title_original TEXT,
-    release_date SMALLINT NOT NULL
+    release_date SMALLINT NOT NULL,
+    end_date SMALLINT NULL,
+    m_type TEXT,
+    is_adult BOOLEAN
 );
+
 CREATE TABLE plays (
     role_id SERIAL PRIMARY KEY,
     role_name VARCHAR(255) NOT NULL,
     movie_id INTEGER NULL,
 	actor_id INTEGER NULL,
-		CONSTRAINT fk_plays_movie_id
-		FOREIGN KEY (movie_id)
-		REFERENCES movies (movie_id)
-		ON DELETE SET NULL,
-		CONSTRAINT fk_plays_actor_id
+    CONSTRAINT fk_plays_movie_id
+        FOREIGN KEY (movie_id)
+        REFERENCES movies (movie_id)
+        ON DELETE SET NULL,
+    CONSTRAINT fk_plays_actor_id
 		FOREIGN KEY (actor_id)
 		REFERENCES actors (actor_id)
 		ON DELETE SET NULL
@@ -32,27 +36,39 @@ CREATE TABLE photos (
     movie_id INTEGER NULL,
     actor_id INTEGER NULL,
     role_id INTEGER NULL,
-		CONSTRAINT fk_photos_movie_id
+	CONSTRAINT fk_photos_movie_id
 		FOREIGN KEY (movie_id)
 		REFERENCES movies (movie_id)
 		ON DELETE SET NULL,
-		CONSTRAINT fk_photos_actor_id
+	CONSTRAINT fk_photos_actor_id
 		FOREIGN KEY (actor_id)
 		REFERENCES actors (actor_id)
 		ON DELETE SET NULL,
-		CONSTRAINT fk_photos_role_id
+	CONSTRAINT fk_photos_role_id
 		FOREIGN KEY (role_id)
 		REFERENCES plays (role_id)
 		ON DELETE SET NULL
 );
 
 CREATE TABLE directors (
-        actor_id INTEGER NOT NULL,
-        movie_id INTEGER NOT NULL,
-        CONSTRAINT PK_directors PRIMARY KEY (actor_id, movie_id)
-    );
+    actor_id INTEGER,
+    movie_id INTEGER,
+    CONSTRAINT fk_directors_movie_id
+        FOREIGN KEY (movie_id)
+        REFERENCES movies (movie_id)
+        ON DELETE SET NULL,
+    CONSTRAINT fk_directors_actor_id
+        FOREIGN KEY (actor_id)
+        REFERENCES actors (actor_id)
+        ON DELETE SET NULL,
+    CONSTRAINT fk_directors PRIMARY KEY (actor_id, movie_id)
+);
 
 CREATE TABLE genre (
-        movie_id INTEGER NOT NULL,
-        genre TEXT NOT NULL
-    );
+    movie_id INTEGER NOT NULL,
+    genre TEXT NOT NULL,
+    CONSTRAINT fk_genre_movie_id
+        FOREIGN KEY (movie_id)
+        REFERENCES movies (movie_id)
+        ON DELETE SET NULL
+);
