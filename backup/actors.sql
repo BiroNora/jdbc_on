aeb2b4c6e4931eb38112134c29b80bfa74e2e702
BiroNora <52859737+BiroNora@users.noreply.github.com>
2022-07-14 18:48:47 +0200

    Remove backup data

--- backup/actors.sql
+++
@@ -1,8 +0,0 @@
-1	Greg Kinnear	1963	\N
-2	Giovanni Ribisi	1974	\N
-3	Johnny Depp	1963	\N
-4	Al Pacino	1940	\N
-5	Jack Nicholson	1937	\N
-6	Mads Mikkelsen	1965	\N
-7	Sigourney Weaver	1949	\N
-8	John Casale	1935	1978
--- backup/movies.sql
+++
@@ -1,5 +0,0 @@
-1	Avatar	Avatar	2009	f
-2	A Karib-tenger kalózai: Ismeretlen vizeken	Pirates of the Caribbean: On Stranger Tides	2011	t
-3	Kánikulai délután	Dog Day Afternoon	1975	t
-4	Rossz álmok	The Gift	2001	t
-5	Lesz ez még így se	 As Good As It Gets	1998	t
