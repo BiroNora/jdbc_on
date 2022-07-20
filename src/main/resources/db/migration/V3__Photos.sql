13df6f372e7b200603096d947e825a46b1ca1bb4
BiroNora <52859737+BiroNora@users.noreply.github.com>
2022-07-20 19:25:17 +0200

    Reduce photos database

---
+++ src/main/resources/db/migration/V3__Photos.sql
@@ -0,0 +1,139 @@
+DELETE FROM photos;
+
+INSERT INTO photos(url, movie_id) VALUES('https://fictional.com/movie/images/6604.jpg', 6604); -- Frédi és Béni, avagy a két kőkorszaki szaki
+INSERT INTO photos(url, movie_id) VALUES('https://fictional.com/movie/images/9918.jpg', 9918); -- A keresztapa
+INSERT INTO photos(url, movie_id) VALUES('https://fictional.com/movie/images/10521.jpg', 10521); -- A keresztapa II
+INSERT INTO photos(url, movie_id) VALUES('https://fictional.com/movie/images/10805.jpg', 10805); -- Kánikulai délután
+INSERT INTO photos(url, movie_id) VALUES('https://fictional.com/movie/images/11402.jpg', 11402); -- Annie Hall
+INSERT INTO photos(url, movie_id) VALUES('https://fictional.com/movie/images/11753.jpg', 11753); -- Családi tűzfészek
+INSERT INTO photos(url, movie_id) VALUES('https://fictional.com/movie/images/13369.jpg', 13369); -- Panelkapcsolat
+INSERT INTO photos(url, movie_id) VALUES('https://fictional.com/movie/images/19181.jpg', 19181); -- Bronxi mese
+INSERT INTO photos(url, movie_id) VALUES('https://fictional.com/movie/images/20554.jpg', 20554); -- Sátántangó
+INSERT INTO photos(url, movie_id) VALUES('https://fictional.com/movie/images/31419.jpg', 31419); -- Werckmeister harmóniák
+INSERT INTO photos(url, movie_id) VALUES('https://fictional.com/movie/images/33479.jpg', 33479); -- Macbeth
+INSERT INTO photos(url, movie_id) VALUES('https://fictional.com/movie/images/36947.jpg', 36947); -- A Karib-tenger kalózai: Holtak kincse
+INSERT INTO photos(url, movie_id) VALUES('https://fictional.com/movie/images/37030.jpg', 37030); -- Állj ki a szerelemért
+INSERT INTO photos(url, movie_id) VALUES('https://fictional.com/movie/images/48674.jpg', 48674); -- A Karib-tenger kalózai: Ismeretlen vizeken
+INSERT INTO photos(url, movie_id) VALUES('https://fictional.com/movie/images/52808.jpg', 52808); -- Cinemarxisme
+INSERT INTO photos(url, movie_id) VALUES('https://fictional.com/movie/images/53855.jpg', 53855); -- A Karib-tenger kalózai: Salazar bosszúja
+INSERT INTO photos(url, movie_id) VALUES('https://fictional.com/movie/images/60412.jpg', 60412); -- Az ajándék
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/493.jpg', 493); -- James Caan
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/549.jpg', 549); -- Greg Kinnear
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/831.jpg', 831); -- Mads Mikkelsen
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/999.jpg', 999); -- Keanu Reeves
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/1480.jpg', 1480); -- Andy Garcia
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/1940.jpg', 1940); -- Jack Nicholson
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/2926.jpg', 2926); -- Donald Sutherland
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/3469.jpg', 3469); -- Carol Kane
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/3806.jpg', 3806); -- Ian McShane
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/4034.jpg', 4034); -- Nicolas Bro
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/4716.jpg', 4716); -- Bea Benaderet
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/5032.jpg', 5032); -- Diane Keaton
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/7335.jpg', 7335); -- Jim Sturgess
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/9143.jpg', 9143); -- Francis Capra
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/9293.jpg', 9293); -- Tom Poston
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/9510.jpg', 9510); -- Hédi Temessy
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/9527.jpg', 9527); -- Rick Moranis
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/9604.jpg', 9604); -- Hal Smith
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/10347.jpg', 10347); -- Rosie O'Donnell
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/10566.jpg', 10566); -- William Hanna
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/11815.jpg', 11815); -- John Stephenson
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/14595.jpg', 14595); -- Marlon Brando
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/17008.jpg', 17008); -- Ralph James
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/20125.jpg', 20125); -- Giovanni Ribisi
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/20607.jpg', 20607); -- László feLugossy
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/23058.jpg', 23058); -- Lars Rudolph
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/23145.jpg', 23145); -- Éva Almássy Albert
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/23496.jpg', 23496); -- Miklós Székely B.
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/26569.jpg', 26569); -- Garry Marshall
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/27286.jpg', 27286); -- Michael Berryman
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/29923.jpg', 29923); -- John Cazale
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/31945.jpg', 31945); -- Sully Boyar
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/37814.jpg', 37814); -- Lillo Brancato
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/42895.jpg', 42895); -- Kyri Ambrus
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/43761.jpg', 43761); -- Élodie Chérie
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/50164.jpg', 50164); -- András Szabó
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/59107.jpg', 59107); -- Mihály Kormos
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/59786.jpg', 59786); -- Gerry Johnson
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/91148.jpg', 91148); -- Johan Arnesson
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/94078.jpg', 94078); -- Sidney Lumet
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/95560.jpg', 95560); -- Peter Weir
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/95861.jpg', 95861); -- Bob Claver
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/95996.jpg', 95996); -- Jeff Chambers
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/96625.jpg', 96625); -- Sam Raimi
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/96787.jpg', 96787); -- Tony Gatlif
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/96791.jpg', 96791); -- Aki Kaurismäki
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/97518.jpg', 97518); -- Brian Levant
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/97599.jpg', 97599); -- Stijn Coninx
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/100576.jpg', 100576); -- Sasa Gedeon
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/100985.jpg', 100985); -- Sharunas Bartas
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/101410.jpg', 101410); -- Gore Verbinski
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/101664.jpg', 101664); -- Marc Dorcel
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/103012.jpg', 103012); -- Andy Bausch
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/106758.jpg', 106758); -- Damjan Kozole
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/107265.jpg', 107265); -- Christoffer Boe
+INSERT INTO photos(url, actor_id) VALUES('https://fictional.com/actor/images/107961.jpg', 107961); -- Rob Marshall
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/1.jpg', 54706, 2926, 1); -- Billy Whistler
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/3.jpg', 54706, 3032, 3); -- Virgil Oldman
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/4.jpg', 54706, 7335, 4); -- Robert
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/6.jpg', 20113, 9527, 6); -- Barney Rubble
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/7.jpg', 48950, 12657, 7); -- Ohlsdorfer's daughter
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/10.jpg', 34902, 6287, 10); -- Elizabeth Swann
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/12.jpg', 48674, 3806, 12); -- Blackbeard
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/14.jpg', 10521, 15718, 14); -- Vito Corleone
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/16.jpg', 10805, 3785, 16); -- Sonny
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/20.jpg', 34902, 8079, 20); -- Jack Sparrow
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/22.jpg', 36947, 22239, 22); -- Will Turner
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/23.jpg', 20554, 23145, 23); -- Schmidtné
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/24.jpg', 48674, 2518, 24); -- Angelica
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/26.jpg', 17313, 1480, 26); -- Vincent Mancini
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/28.jpg', 11402, 25746, 28); -- Rob
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/29.jpg', 39410, 8079, 29); -- Jack Sparrow
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/30.jpg', 13369, 44, 30); -- Férj
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/31.jpg', 10920, 27286, 31); -- Ellis
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/34.jpg', 10521, 3785, 34); -- Michael
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/36.jpg', 38075, 12657, 36); -- Henriette
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/37.jpg', 9918, 3785, 37); -- Michael
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/38.jpg', 53855, 8079, 38); -- Captain Jack Sparrow
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/39.jpg', 9918, 5032, 39); -- Kay Adams
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/41.jpg', 38218, 831, 41); -- Ivan
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/43.jpg', 14153, 16970, 43); -- Tibor, a tanár úr
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/44.jpg', 13079, 40396, 44); -- Balázs
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/45.jpg', 10920, 40526, 45); -- Col. Matterson
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/46.jpg', 16594, 6246, 46); -- John Keating
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/47.jpg', 38218, 15320, 47); -- Sarah Svendsen
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/48.jpg', 35508, 9674, 48); -- Edward Wilson
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/50.jpg', 13369, 42895, 50); -- énekesnõ
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/58.jpg', 20113, 1475, 58); -- Fred Flintstone
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/59.jpg', 38075, 4049, 59); -- Brown
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/60.jpg', 60412, 12523, 60); -- Lucy
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/62.jpg', 23782, 49115, 62); -- Eva","Rémy's wife--Mathilde Rémy's late wife
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/64.jpg', 39410, 22239, 64); -- Will Turner
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/66.jpg', 13369, 1902, 66); -- Feleség
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/67.jpg', 31419, 50160, 67); -- György Eszter
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/72.jpg', 16594, 18323, 72); -- Knox Overstreet
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/74.jpg', 6604, 14883, 74); -- Wilma Flintstone","Pebbles Flintstone","Nurse
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/76.jpg', 16128, 53825, 76); -- Willarsky","bartender
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/78.jpg', 31419, 4049, 78); -- Man In The Broad-Cloth Coat
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/81.jpg', 6604, 299, 81); -- Barney Rubble","Dino","Judge
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/83.jpg', 11753, 58699, 83); -- Gabi, Laci testvére
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/88.jpg', 6604, 59786, 88); -- Betty Rubble","Additional Voices","Cuckoo
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/91.jpg', 10521, 2, 91); -- Tom Hagen
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/98.jpg', 20554, 42657, 98); -- Petrina
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/103.jpg', 11668, 9293, 103); -- Mr. Bickley
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/111.jpg', 9918, 493, 111); -- Sonny
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/116.jpg', 36947, 8079, 116); -- Jack Sparrow
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/118.jpg', 11668, 34022, 118); -- Mindy McConnell","Mandy
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/120.jpg', 34902, 3032, 120); -- Barbossa
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/123.jpg', 53855, 22239, 123); -- Will Turner
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/125.jpg', 38075, 7864, 125); -- Maloin, az éjszakai váltóõr
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/126.jpg', 35508, 15718, 126); -- Bill Sullivan
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/128.jpg', 17313, 3785, 128); -- Michael Corleone
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/130.jpg', 38491, 84888, 130); -- Narrator (segment \"Sweden: The Yellow Tag\")
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/131.jpg', 20113, 5454, 131); -- Miss Stone
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/132.jpg', 48950, 88647, 132); -- Horse
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/133.jpg', 60412, 29382, 133); -- Simon
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/134.jpg', 38075, 5700, 134); -- Maloin felesége
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/137.jpg', 11753, 90642, 137); -- Laci apja
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/141.jpg', 13079, 91214, 141); -- Kata
+INSERT INTO photos(url, movie_id, actor_id, role_id) VALUES('https://fictional.com/role/images/143.jpg', 38218, 4034, 143); -- Gunnar
