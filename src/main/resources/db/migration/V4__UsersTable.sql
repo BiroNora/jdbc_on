74b6cf83815ab86cb8b9b0fef34c66c6e35b3679
BiroNora <52859737+BiroNora@users.noreply.github.com>
2022-08-06 13:01:53 +0200

    User docker in progress

--- src/main/resources/db/migration/V4__UsersTable.sql
+++ src/main/resources/db/migration/V4__UsersTable.sql
@@ -2,7 +2,7 @@ CREATE EXTENSION "uuid-ossp";
 
 CREATE TABLE users (
     user_id uuid NOT NULL DEFAULT uuid_generate_v1() PRIMARY KEY,
-    username VARCHAR NULL UNIQUE,
-    email VARCHAR NOT NULL UNIQUE,
-    password VARCHAR NOT NULL
+    username VARCHAR(50) NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(50) NOT NULL
 );
