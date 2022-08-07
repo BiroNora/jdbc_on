6b0ee0498d8e6fd12b2cd6360be65a6e11acbaa8
BiroNora <52859737+BiroNora@users.noreply.github.com>
2022-08-07 13:23:40 +0200

    Users table added (instead of user database)

--- src/main/java/com/norab/security/SecurityConfig.java
+++ src/main/java/com/norab/security/SecurityConfig.java
@@ -21,15 +21,26 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-            .antMatchers("/", "index", "/css/*", "/js/*")
-            .permitAll()
+            .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
             .anyRequest()
             .authenticated()
             .and()
-            .httpBasic();
+            .formLogin();
 
         return http.build();
     }
+
+
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//            .csrf().disable()
+//            .authorizeRequests()
+//            .antMatchers("/api/v*/registration/**")
+//            .permitAll()
+//            .anyRequest()
+//            .authenticated().and()
+//            .formLogin();
+//    }
 
     /*@Bean
     public WebSecurityCustomizer webSecurityCustomizer() throws Exception{
